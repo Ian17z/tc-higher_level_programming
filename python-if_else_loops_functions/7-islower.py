593d5a3872fb46e24760e4c4e9365a491e75ca0a
ian <ian@gmail.com>
2025-12-05 04:56:19 +0000

    commit

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-print("a is {}".format("lower" if islower("a") == True else "upper"))
-print("H is {}".format("lower" if islower("H") == True else "upper"))
-print("A is {}".format("lower" if islower("A") == True else "upper"))
-print("3 is {}".format("lower" if islower("3") == True else "upper"))
-print("g is {}".format("lower" if islower("g") == True else "upper"))
+    if c >= 'a' and c <= 'z':
+        return True
+    return False
+print(islower('a'))  # True
+print(islower('Z'))  # False
+print(islower('m'))  # True
+print(islower('1'))  # False
