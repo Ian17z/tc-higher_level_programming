e7463339818ba223ea645188e8da9fbd736c1051
ian <ian@gmail.com>
2025-12-05 05:09:42 +0000

    s

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     return abs(number) % 10
-print_last_digit(98)
-print_last_digit(0)
+r = print_last_digit(98)
+print(r)
+r = print_last_digit(0)
+print(r)
 r = print_last_digit(-1024)
 print(r)
