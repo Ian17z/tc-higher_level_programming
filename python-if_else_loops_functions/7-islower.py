affcc741277e377c25a56ef9797ab134feb51d49
ian <ian@gmail.com>
2025-12-05 05:41:37 +0000

    pfvr

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 123:
+    if c>= 97 and c <= 122:
         return True
     else:
         return False
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+print("a is {}".format("lower" if islower(97) else "upper"))
+print("H is {}".format("lower" if islower(72) else "upper"))
+print("A is {}".format("lower" if islower(65) else "upper"))
+print("3 is {}".format("lower" if islower(51) else "upper"))
+print("g is {}".format("lower" if islower(103) else "upper"))
