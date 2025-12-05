8081e4b92562ddad9d58995491ab35ab48971100
ian <ian@gmail.com>
2025-12-05 08:33:21 +0000

    boa

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     result = ""
     for c in str:
-        if c >= 'a' and c <= 'z':
+        if 'a' <= c <= 'z':
             result += chr(ord(c) - 32)
         else:
             result += c
