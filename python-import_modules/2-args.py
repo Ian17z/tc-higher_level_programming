044dd9e6afd86a447716f41990b8580938993319
Ian <ian@gmail.com>
2025-12-17 04:09:12 +0000

    wow

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__  == "__main__":
+    argv = input('Digite um valor: ').split()
+    if len(argv) == 1:
+        print('1 argument: \n1: {}'.format(argv[0]))
+    elif len(argv) > 1:
+        print('{} arguments: '.format(len(argv)))
+        for i in range(len(argv)):
+            print('{}: {}'.format(i + 1, argv[i]))
+    else:
+        print('0 arguments.')
