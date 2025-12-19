4c155c3ea98e2710b9f100eebd8115321959154f
ian <ian@gmail.com>
2025-12-19 14:10:24 +0000

    wow

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
 if __name__  == "__main__":
-    argv = input().split()
-    if len(argv) == 1:
-        print('1 argument:\n1: {}'.format(argv[0]))
-    elif len(argv) > 1:
-        print('{} arguments: '.format(len(argv)))
-        for i in range(len(argv)):
-            print('{}: {}'.format(i + 1, argv[i]))
+    import sys
+    if (len(sys.argv) - 1) == 1:
+        print('1 argument:')
+        print('1: {}'.format(sys.argv[1]))
+    elif (len(sys.argv) - 1) > 1:
+        print('{} arguments:'.format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print('{}: {}'.format(i, sys.argv[i]))
     else:
         print('0 arguments.')
