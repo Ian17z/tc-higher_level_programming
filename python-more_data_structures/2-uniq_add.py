d9bce746acd8816193eb3a4ed7c44b5a21b0d226
ian <ian@gmail.com>
2026-01-22 00:36:12 +0000

    commit

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    total = 0
+    for i in range(len(my_list)):
+        if my_list[i] not in my_list[:i]:
+            total += my_list[i]
+
+    return total
