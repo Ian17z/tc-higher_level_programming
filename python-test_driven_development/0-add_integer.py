49cb5e7b14474fbed643cc28726569d4b02e1469
ian <ian@gmail.com>
2026-02-02 23:32:14 +0000

    ian

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+'''
+Este módulo devolve uma função que é capaz de fazer a soma inteira de dois números inteiros ou flutuantes.
+'''
 def add_integer(a, b=98):
+    '''
+    Try: Soma dois números inteiros ou flutuantes.
+    
+    Execept: Caso a ou b não se enquadrem na função
+    '''
     try:
         a = int(a)
     except (TypeError, ValueError):
---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    '''Divisão de matrizes.'''
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number") 
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not isinstance(matrix, list) or not matrix or not isinstance(matrix[0], list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    tamanho_linha = len(matrix[0])
+    nova_matriz = []
+    for linha in matrix:
+        if len(linha) != tamanho_linha:
+            raise TypeError("Each row of the matrix must have the same size")
+        
+        nova_linha = []
+        for elemento in linha:
+            if not isinstance(elemento, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            resultado = round(elemento / div, 2)
+            nova_linha.append(resultado)
+        nova_matriz.append(nova_linha)
+    return nova_matriz
