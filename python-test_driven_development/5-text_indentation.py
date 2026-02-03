27ef8f7e5afa79eac99eeb6878b126ee6287cea2
ian <ian@gmail.com>
2026-02-03 00:11:55 +0000

    ian

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Printa um texto com 2 novas linhas a cada ., ? ou : na frase digitada.
+"""
+def text_indentation(text):
+    '''
+    A cada ., ? ou : é gerada 2 novas linhas.
+
+    Se não, é necessário digitar uma string ou algo
+    '''
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for char in text:
+        if char in ".?:":
+            print(char)
+            print()
+        else:
+            print(char, end="")
