b87aa4d3f6e640819e4001f4e40a98f7fd56e0c6
ian <ian@gmail.com>
2026-02-12 02:55:03 +0000

    commit ex 19

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+from functools import reduce
 def calc_average(a_dictionary):
-    print('nao faço ideia')
+    n = len(a_dictionary)
+
+    print(reduce(lambda a, b: a + b["age"], a_dictionary, 0) / n)
+    print(reduce(lambda a, b: a + b["salary"], a_dictionary, 0) / n)
