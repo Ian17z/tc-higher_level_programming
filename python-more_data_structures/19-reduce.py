14a036d1e1c2c7230329eaf3990059e949a52fc3
ian <ian@gmail.com>
2026-02-12 03:07:47 +0000

    commit ex 19

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -7,5 +7,3 @@ def calc_average(a_dictionary):
     totalsalario = reduce(lambda acc, emp: acc + emp["salary"], valores, 0)
     mediaidade = totalidade / n
     averagesalario = totalsalario / n
-
-    print(f"The average salary is R${average_salary:.2f} with an average age of {average_age:.2f}")
