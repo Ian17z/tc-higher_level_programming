da5fefc709213e0fad9c628a7788fcfc4d2c0a16
ian <ian@gmail.com>
2026-02-12 00:23:32 +0000

    commit ex 17

--- python-more_data_structures/17-filtering_data.py
+++ python-more_data_structures/17-filtering_data.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def filtering_data(a_dictionary):
-    return list(
-        map(lambda x: x['name'],
-            filter(lambda x: x['salary'] > 10000, a_dictionary)
-        )
-    )
+    return list(map(lambda emp: emp["name"],
+                    filter(lambda emp: emp["salary"] > 10000, a_dictionary)))
