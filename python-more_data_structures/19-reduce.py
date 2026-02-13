c7979671fadab893af0de9bf853d22886a21ae5c
ian <ian@gmail.com>
2026-02-13 01:52:12 +0000

    ian

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 from functools import reduce
-#!/usr/bin/python3
-from functools import reduce
-def calc_average(data):
-    values = list(data.values())
-    total = len(values)
-    total_salary = reduce(lambda acc, x: acc + x["salary"], values, 0)
-    total_age = reduce(lambda acc, x: acc + x["age"], values, 0)
-    avg_salary = total_salary / total
-    avg_age = total_age / total
-    print(
-        f"The average salary is R${avg_salary:.2f} "
-        f"with an average age of {avg_age:.2f}"
-    )
+def calc_average(a_dictionary):
+    total_salary = reduce(lambda acc, x: acc + x["salary"], a_dictionary, 0)
+    total_age = reduce(lambda acc, x: acc + x["age"], a_dictionary, 0)
+    average_salary = total_salary / len(a_dictionary)
+    average_age = total_age / len(a_dictionary)
+    return average_salary, average_age
