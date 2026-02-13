e25a4d18774b636fc5d563e98df6b13af9b1cbfb
ian <ian@gmail.com>
2026-02-13 01:45:09 +0000

    commit ex 19

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python3
 from functools import reduce
-
-
-def calc_average(a_dictionary):
-    """Calcula a média de age e salary usando reduce"""
-
-    values = a_dictionary.values()
-    count = len(values)
-
-    total_salary = reduce(
-        lambda acc, item: acc + item["salary"],
-        values,
-        0
-    )
-
-    total_age = reduce(
-        lambda acc, item: acc + item["age"],
-        values,
-        0
-    )
-
-    average_salary = total_salary / count if count else 0
-    average_age = total_age / count if count else 0
-
-    return average_salary, average_age
+def media_age_salary(data):
+    valores = [data.get("age", 0), data.get("salary", 0)]
+    media = reduce(lambda x, y: x + y, valores) / len(valores)
+    print(media)
