929387ab7da0a8b1f0d38a0fa453195bb1bb4ec6
ian <ian@gmail.com>
2026-02-13 01:42:41 +0000

    commit ex 19

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 from functools import reduce
 def calc_average(a_dictionary):
-    values = list(a_dictionary.values())
-    n = len(values)
-    total_age = reduce(lambda acc, emp: acc + emp.get("age", 0), values, 0)
-    total_salary = reduce(lambda acc, emp: acc + emp.get("salary", 0), values, 0)
-    average_age = total_age / n
-    average_salary = total_salary / n
+    count = len(a_dictionary)
+    total_salary = reduce(
+        lambda acc, item: acc + item["salary"],
+        a_dictionary,
+        0
+    )
+    total_age = reduce(
+        lambda acc, item: acc + item["age"],
+        a_dictionary,
+        0
+    )
+    average_salary = total_salary / count if count else 0
+    average_age = total_age / count if count else 0
     return average_salary, average_age
