78ae4d6adb8411fff8f33b2c11d77b9604f593e4
ian <ian@gmail.com>
2026-02-13 02:01:40 +0000

    ex 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(string_romana, str):
+def roman_to_int(roman_string: str) -> int:
+    if not isinstance(roman_string, str):
         return 0
-    mapa_romano = {
+    roman_map = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
     total = 0
-    valor_anterior = 0
-    for caractere in reversed(string_romana.upper()):
-        valor_atual = mapa_romano.get(caractere, 0)
-        if valor_atual < valor_anterior:
-            total -= valor_atual
+    prev_value = 0
+    for char in reversed(roman_string.upper()):
+        current_value = roman_map.get(char, 0)
+        if current_value < prev_value:
+            total -= current_value
         else:
-            total += valor_atual    
-        valor_anterior = valor_atual
+            total += current_value 
+        prev_value = current_value
     return total
