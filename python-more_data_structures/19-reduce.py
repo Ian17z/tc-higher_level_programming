09a6d2fa0c8948302155216094897ccd95c4459e
ian <ian@gmail.com>
2026-02-13 01:46:03 +0000

    ian

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 from functools import reduce
-def media_age_salary(data):
-    valores = [data.get("age", 0), data.get("salary", 0)]
-    media = reduce(lambda x, y: x + y, valores) / len(valores)
-    print(media)
+#!/usr/bin/python3
+from functools import reduce
+def calc_average(data):
+    values = list(data.values())
+    total = len(values)
+    total_salary = reduce(lambda acc, x: acc + x["salary"], values, 0)
+    total_age = reduce(lambda acc, x: acc + x["age"], values, 0)
+    avg_salary = total_salary / total
+    avg_age = total_age / total
+    print(
+        f"The average salary is R${avg_salary:.2f} "
+        f"with an average age of {avg_age:.2f}"
+    )
