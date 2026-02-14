ad06d74bb4b9caca36d2cac633ff62f1efe7bfd9
ian <ian@gmail.com>
2026-02-14 00:27:38 +0000

    vai pfvr

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -1,20 +1,25 @@
-#!/usr/bin/python3
 from functools import reduce
 
-def calc_average(a_dictionary):
-    if not a_dictionary:
+def calc_average(a_list_of_dictionaries):
+    if not a_list_of_dictionaries:
         return 0, 0
 
-    # O segredo é o terceiro argumento: {'age': 0, 'salary': 0}
-    # Isso garante que 'acc' seja SEMPRE um dicionário desde o início
-    somas = reduce(
+    # Usamos o reduce para somar 'age' e 'salary'
+    # O terceiro argumento {} é o valor inicial do acumulador (acc)
+    totals = reduce(
         lambda acc, curr: {
             'age': acc['age'] + curr.get('age', 0),
             'salary': acc['salary'] + curr.get('salary', 0)
         },
-        a_dictionary,
+        a_list_of_dictionaries,
         {'age': 0, 'salary': 0}
     )
 
-    n = len(a_dictionary)
-    return (somas['salary'] / n, somas['age'] / n)
+    n = len(a_list_of_dictionaries)
+    avg_age = totals['age'] / n
+    avg_salary = totals['salary'] / n
+
+    # O output desejado pelo seu sistema de correção:
+    print(f"The average salary is R${avg_salary:.2f} with an average age of {avg_age}")
+    
+    return avg_salary, avg_age
