3413125077c7f21694bc748f1f87a1911d16158e
ian <ian@gmail.com>
2026-02-14 00:54:30 +0000

    ian

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-#!/usr/bin/env python3
 from functools import reduce
 
 def calc_average(a_dictionary):
     """
     Calcula a média dos salários e idades de uma lista de dicionários
     """
-    # Verificação simples: se não for lista, retorna (0,0)
-    if not isinstance(a_dictionary, list):
+    try:
+        # Soma todos os salários usando reduce
+        total_salary = reduce(lambda acc, person: acc + person['salary'], a_dictionary, 0)
+        
+        # Soma todas as idades usando reduce
+        total_age = reduce(lambda acc, person: acc + person['age'], a_dictionary, 0)
+        
+        # Calcula as médias
+        n = len(a_dictionary)
+        return (total_salary / n, total_age / n)
+    except:
+        # Se algo der errado, retorna (0, 0)
         return (0, 0)
-    
-    # Se a lista estiver vazia
-    if len(a_dictionary) == 0:
-        return (0, 0)
-    
-    # Usando reduce para somar salários
-    total_salary = 0
-    total_age = 0
-    
-    for person in a_dictionary:
-        if isinstance(person, dict):
-            total_salary += person.get('salary', 0)
-            total_age += person.get('age', 0)
-    
-    n = len(a_dictionary)
-    return (total_salary / n, total_age / n)
